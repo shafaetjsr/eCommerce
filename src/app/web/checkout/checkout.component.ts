eda2c900f37adb366a1d550aecb0ccb0806e2c9f
Joherul Hoque <florasystem17@gmail.com>
2024-08-13 10:28:21 +0600

    Added New shipping Address

--- src/app/web/checkout/checkout.component.ts
+++ src/app/web/checkout/checkout.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  
+  showForm1: boolean = true; 
+
+  showForm2() {
+    this.showForm1 = false;
+  }
+
+  showFsorm1() {
+    this.showForm1 = true;
+  }
 
 }
